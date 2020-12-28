047aa547a85da101db114b8789965e4d57c13b62
Sasha Donchak <pracadonchak@gmail.com>
2020-12-28 23:04:39 +0300

    Created context for alert

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Alert from './components/Alert';
 import { Navbar } from './components/Navbar';
+import AlertState from './context/alert/AlertState';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <BrowserRouter>
-    <Navbar/>
-      <div className="container pt-4">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
+    <AlertState>
+      <BrowserRouter>
+        <Navbar/>
+          <div className="container pt-4">
+            <Alert/>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/about">
+                <About />
+              </Route>
 
-        </Switch>
-      </div>
-    </BrowserRouter>
+            </Switch>
+          </div>
+      </BrowserRouter>
+    </AlertState>
   );
 }
 
