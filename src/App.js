49e2c5fbf1cd3215012c3dc99a56b5548ef5b723
Sasha Donchak <pracadonchak@gmail.com>
2020-12-28 20:57:17 +0300

    Added home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
+    <Navbar/>
       <div className="container pt-4">
-        <h1>
-          Hello, React!
-        </h1>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+
+        </Switch>
       </div>
     </BrowserRouter>
   );
