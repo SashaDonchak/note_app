a8bb716f1fd8635990c9f54d7a33447265850a9b
Sasha Donchak <pracadonchak@gmail.com>
2020-12-29 23:52:30 +0300

    Added animations and deployed 1.1

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { useState, useContext } from 'react';
+import { AlertContext } from '../context/alert/alertContext';
+import { FirebaseContext } from '../context/firebase/firebaseContext';
+
+const Form = () => {
+
+    const [value, setValue] = useState('');
+
+    const alert = useContext(AlertContext);
+
+    const fireBase = useContext(FirebaseContext);
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        if (value.trim()) {
+            
+            setValue('');
+
+            fireBase.addNote(value.trim())
+                .then(() => {
+                    alert.show('New note was created', 'success');
+                    
+                })
+                .catch(() => {
+                    alert.show('Something went wrong', 'danger');
+                });
+
+        } else {
+            alert.show('Enter note title');
+        }
+        
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <div className="form-group">
+                <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Enter note"
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                />
+            </div>
+        </form>
+    );
+};
+
+export default Form;
---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => (
+    <div className="jumbotron">
+        <div className="container">
+            <h1 className="display-4">The best react app</h1>
+            <p className="lead">App version - <strong>1.1</strong></p>
+        </div>
+    </div>
+)
