21b0b190455155b68400638b3c8bfa409309848c
Sasha Donchak <pracadonchak@gmail.com>
2020-12-29 17:26:43 +0300

    Added firebase and logic

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Alert from './components/Alert';
 import { Navbar } from './components/Navbar';
 import AlertState from './context/alert/AlertState';
+import FirebaseState from './context/firebase/FirebaseState';
 import { About } from './pages/About';
 import { Home } from './pages/Home';
 
 function App() {
   return (
-    <AlertState>
-      <BrowserRouter>
-        <Navbar/>
+    <FirebaseState>
+      <AlertState>
+        <BrowserRouter>
+          <Navbar />
           <div className="container pt-4">
-            <Alert/>
+            <Alert />
             <Switch>
               <Route exact path="/">
                 <Home />
@@ -22,8 +24,9 @@ function App() {
 
             </Switch>
           </div>
-      </BrowserRouter>
-    </AlertState>
+        </BrowserRouter>
+      </AlertState>
+    </FirebaseState>
   );
 }
 
