65562df240f160f879fa2c5a4d9ca9cd361d7034
Sasha Donchak <pracadonchak@gmail.com>
2020-12-30 12:48:16 +0300

    Fixed bug with null notes

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,30 +1,34 @@
 import React from 'react';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
-const Notes = ({ notes, onRemove }) => (
-    <TransitionGroup component="ul" className="list-group">
-        {notes.map(note => (
-            <CSSTransition
-                key={note.id}
-                classNames="note"
-                timeout={800}
-            >
-                <li className="list-group-item note">
-                    <div>
-                        <strong>
-                            {note.title}
-                        </strong>
-                        <span>
-                            {note.date}
-                        </span>
-                    </div>
-                    <button onClick={() => onRemove(note.id)} className="btn btn-outline-danger btn-sm">
-                        &times;
-                    </button>
-                </li>
-            </CSSTransition>
-        ))}
-    </TransitionGroup>
-);
+const Notes = ({ notes, onRemove }) => {
+    if (notes === null) return;
+    
+    return(
+        <TransitionGroup component="ul" className="list-group">
+            {notes.map(note => (
+                <CSSTransition
+                    key={note.id}
+                    classNames="note"
+                    timeout={800}
+                >
+                    <li className="list-group-item note">
+                        <div>
+                            <strong>
+                                {note.title}
+                            </strong>
+                            <span>
+                                {note.date}
+                            </span>
+                        </div>
+                        <button onClick={() => onRemove(note.id)} className="btn btn-outline-danger btn-sm">
+                            &times;
+                        </button>
+                    </li>
+                </CSSTransition>
+            ))}
+        </TransitionGroup>
+    );
+};
 
 export default Notes;
