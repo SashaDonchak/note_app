e1d49f3e8412817ae2d89bc7e57520476afdc1cf
Sasha Donchak <pracadonchak@gmail.com>
2021-01-08 00:50:37 +0300

    Added signin

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,24 +1,45 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import app from '../config';
 
-const Signin = () => (
-  <form>
-    <div className="form-group">
-      <label htmlFor="user_email">Email address</label>
-      <input
-        type="email"
-        className="form-control"
-        id="user_email"
-        aria-describedby="emailHelp"
-      />
-    </div>
-    <div className="form-group">
-      <label htmlFor="user_password">Password</label>
-      <input type="password" className="form-control" id="user_password" />
-    </div>
-    <button type="submit" className="btn btn-outline-primary">
-      Submit
-    </button>
-  </form>
-);
+const Signin = ({ history }) => {
+  const handleSignin = async (event) => {
+    event.preventDefault();
+    const { userEmail, userPassword } = event.target.elements;
+    try {
+      await app
+        .auth()
+        .signInWithEmailAndPassword(userEmail.value, userPassword.value);
+      history.push('/');
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  };
+  return (
+    <form onSubmit={handleSignin}>
+      <div className="form-group">
+        <label htmlFor="userEmail">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          id="userEmail"
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="userPassword">Password</label>
+        <input type="password" className="form-control" id="userPassword" />
+      </div>
+      <button type="submit" className="btn btn-outline-primary">
+        Submit
+      </button>
+    </form>
+  );
+};
 
-export default Signin;
+Signin.propTypes = {
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default withRouter(Signin);
